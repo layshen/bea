aaa5babd6f1df456b2fc2814437c4d43f12a198e
maoweiyi <1335112997@qq.com>
2023-03-15 22:11:52 +0800

    [opz] 增加多个激励广告广告位

--- app/src/main/java/com/epiboly/bea/advertisement/AdvertisementInit.kt
+++ app/src/main/java/com/epiboly/bea/advertisement/AdvertisementInit.kt
@@ -3,6 +3,7 @@ package com.epiboly.bea.advertisement
 import android.app.Application
 import com.epiboly.bea.rich.R
 import com.ifmvo.togetherad.csj.TogetherAdCsj
+import kotlin.random.Random
 
 /**
  * @author mao
@@ -19,9 +20,14 @@ class AdvertisementInit {
             //初始化广点通
 //            TogetherAdGdt.init(context = context, adProviderType = AdProviderType.GDT.type, gdtAdAppId = "1201369644")
 
+            //950758827
+            val asList = listOf("951570314", "951570316", "950758827","951570356")
+            val random = Random(System.currentTimeMillis());
+            val index = (0..3).random(random)
+            val rewardId :String= asList[index]
             TogetherAdCsj.idMapCsj = mutableMapOf(
                 TogetherAdAlias.AD_SPLASH to "888050848",
-                TogetherAdAlias.AD_REWARD to "950758827"
+                TogetherAdAlias.AD_REWARD to rewardId
 
             )
 
